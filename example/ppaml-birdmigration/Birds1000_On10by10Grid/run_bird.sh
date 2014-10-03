d51f2320dec263b4640e65b52a4f698bab6ee824
lileicc <lileicc@gmail.com>
2014-10-02 22:32:24 -0700

    fix bird model

--- example/ppaml-birdmigration/Birds1000_On10by10Grid/run_bird.sh
+++ example/ppaml-birdmigration/Birds1000_On10by10Grid/run_bird.sh
@@ -1,10 +1,10 @@
+#!/bin/bash
 
 BLOG_HOME=../../../blog
 time "$BLOG_HOME"/blog -r -e blog.engine.LiuWestFilter \
-  LocToInteger.blog \
-  bird_flow_probs.blog \
-  bird_features.blog \
   bird_model.blog \
   bird_obs.blog \
   bird_queries.blog \
-  -Prho=0.95 -n 100 -q 1 -o out.json
+  -Prho=0.95 -n 10000 -q 1 
+
+#-o out.json
